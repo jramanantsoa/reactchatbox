7f17834ba946498f2278eb46455131f17b47067a
Jessica <you@example.com>
2021-05-11 16:10:07 +0200

    Création des components Message Formulaire

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import Formulaire from './components/Formulaire'
+import Message from './components/Message'
 
 class App extends Component {
   render() {
     return (
       <div className="box">
-        <h2>App</h2>
+        <div className="message">
+        <Message></Message>
+        </div>
+        <Formulaire></Formulaire>
       </div>
     );
   }
---
+++ src/components/Message.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Message extends Component {
+    render() {
+      return (
+       <p className='user-message'>
+           Mon message
+       </p>
+      )
+    }
+  }
+  export default Message
